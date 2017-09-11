e0805043d88c4537601bbcb20b5c5f666b8a91d2
huutrung <huutrungbkdn15@gmail.com>
2017-09-11 10:46:04 +0700

    first commit

---
+++ app/src/main/java/com/example/admin/caculator/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.admin.caculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et1, et2;
+    private Button cong, tru, nhan, chia;
+    private TextView result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.et1);
+        et2 = (EditText) findViewById(R.id.et2);
+
+        cong = (Button) findViewById(R.id.cong);
+        tru = (Button) findViewById(R.id.tru);
+        nhan = (Button) findViewById(R.id.nhan);
+        chia = (Button) findViewById(R.id.chia);
+    }
+    public void Tong(View view){
+        double a = Double.parseDouble(et1.getText()+"");
+        double b = Double.parseDouble(et2.getText()+"");
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText((a+b)+"");
+
+    }
+    public void Hieu(View view){
+        double a = Double.parseDouble(et1.getText()+"");
+        double b = Double.parseDouble(et2.getText()+"");
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText((a-b)+"");
+    }
+    public void Tich(View view){
+        double a = Double.parseDouble(et1.getText()+"");
+        double b = Double.parseDouble(et2.getText()+"");
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText((a*b)+"");
+    }
+    public void Thuong(View view){
+        double a = Double.parseDouble(et1.getText()+"");
+        double b = Double.parseDouble(et2.getText()+"");
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText((a/b)+"");
+    }
+}
